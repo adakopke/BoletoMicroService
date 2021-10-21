b20e796ced9c6e01d9175c5ca36fa52301b05a48
adakopke <adakopke@gmail.com>
2021-10-21 18:39:03 -0300

    Spring initialzr

---
+++ src/main/kotlin/br/com/letscode/mybank/msboleto/MsboletoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.letscode.mybank.msboleto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsboletoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsboletoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/letscode/mybank/msboleto/MsboletoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.letscode.mybank.msboleto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsboletoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
